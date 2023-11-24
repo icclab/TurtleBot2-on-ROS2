13a94f44b73ac2ada88c398ec8a281d742d9a902
Sisqui <aries94leifu@gmail.com>
2023-11-24 12:42:21 +0000

    add rtabmap

---
+++ turtlebot2_bringup/launch/rtabmap.launch.py
@@ -0,0 +1,36 @@
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch_ros.substitutions import FindPackageShare
+from launch.substitutions import PathJoinSubstitution
+
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+
+def generate_launch_description():
+
+    return LaunchDescription([
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([
+                PathJoinSubstitution([
+                    FindPackageShare('rtabmap_launch'),
+                    'launch',
+                    'rtabmap.launch.py'
+                ])
+            ]),
+            launch_arguments={
+                'rtabmap_args': "--delete_db_on_start",
+                'compressed':'true',
+                'rgb_topic': '/camera/color/image',
+                'depth_topic': '/camera/depth/image',
+                'camera_info_topic': '/camera/color/camera_info',
+                'visual_odometry': 'false',
+                'frame_id': 'base_footprint',
+                'subscribe_depth': 'true',
+                'approx_sync': 'true',
+                'odom_topic': '/odom',
+                'qos': '2',
+                'args': "-d --RGBD/NeighborLinkRefining true --Reg/Strategy 1",
+                'use_sim_time': 'true'
+            }.items()
+        )
+    ])
