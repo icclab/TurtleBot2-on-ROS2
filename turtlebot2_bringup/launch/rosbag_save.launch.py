049585b35aa44cb07a43ba4d4072aa865fb5d751
Leonardo Militano <39118873+miltzhaw@users.noreply.github.com>
2024-06-12 10:22:15 +0200

    Update rosbag_save.launch.py

--- turtlebot2_bringup/launch/rosbag_save.launch.py
+++ turtlebot2_bringup/launch/rosbag_save.launch.py
@@ -3,6 +3,6 @@ from launch import LaunchDescription
 def generate_launch_description():
     return launch.LaunchDescription([
         launch.actions.ExecuteProcess(
-            cmd=['ros2', 'bag', 'record', '-a', 's mcap', '-d 5', '-o my_bag'],
+            cmd=['ros2', 'bag', 'record', '-a', '-s mcap', '-d 5', '-o my_bag'],
         )
     ])
